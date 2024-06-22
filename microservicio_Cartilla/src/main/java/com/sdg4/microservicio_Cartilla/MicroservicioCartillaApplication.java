bb8ec2daf897baba1f7658265ba5a043e0fccf4f
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-06-22 18:20:12 -0500

    lanzador de la applicacion

---
+++ microservicio_Cartilla/src/main/java/com/sdg4/microservicio_Cartilla/MicroservicioCartillaApplication.java
@@ -0,0 +1,13 @@
+package com.sdg4.microservicio_Cartilla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioCartillaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioCartillaApplication.class, args);
+	}
+
+}
