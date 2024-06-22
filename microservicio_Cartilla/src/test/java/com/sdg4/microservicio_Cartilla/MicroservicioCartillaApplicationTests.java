4d09cc13729a6b476cf34f000b67fe5d7e635d41
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-06-22 18:22:05 -0500

    no se modificoo nada de esto

---
+++ microservicio_Cartilla/src/test/java/com/sdg4/microservicio_Cartilla/MicroservicioCartillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdg4.microservicio_Cartilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioCartillaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
