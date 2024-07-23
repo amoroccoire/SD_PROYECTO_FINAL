a839a0ec740cfb5169764997aa32e613f06941cb
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-22 20:03:34 -0500

    crea el voto y lo guarda en la bd y envia a kafka

--- ms-votes/src/main/java/com/sdg4/votes/services/VoteService.java
+++ ms-votes/src/main/java/com/sdg4/votes/services/VoteService.java
@@ -2,6 +2,7 @@ package com.sdg4.votes.services;
 
 import com.sdg4.votes.models.dto.CreateVote;
 import com.sdg4.votes.models.entities.Vote;
+import com.sdg4.votes.proto.VoteProto;
 import com.sdg4.votes.repositories.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,14 @@ import java.time.LocalDateTime;
 public class VoteService {
     @Autowired
     private VoteRepository voteRepository;
+    @Autowired
+    private MessageService messageService;
+
+    public Mono<Boolean> searchVote(CreateVote vote) {
+        return voteRepository.findByVoterId(vote.voterId())
+                .map(voteFound -> true)
+                .defaultIfEmpty(false);
+    }
 
     public Mono<Vote> createVote(CreateVote vote) {
         Vote voteCreated = new Vote();
@@ -24,6 +33,26 @@ public class VoteService {
         voteCreated.setCandidateId(vote.candidateId());
         voteCreated.setVoterId(vote.voterId());
 
-        return voteRepository.save(voteCreated);
+        return voteRepository.save(voteCreated)
+                .flatMap(savedVote -> {
+                    LocalDateTime now = savedVote.getTimestamp();
+                    VoteProto.LocalDateTime protoVoteTime = VoteProto.LocalDateTime.newBuilder()
+                            .setYear(now.getYear())
+                            .setMonth(now.getMonthValue())
+                            .setDay(now.getDayOfMonth())
+                            .setHour(now.getHour())
+                            .setMinute(now.getMinute())
+                            .setSecond(now.getSecond())
+                            .build();
+
+                    VoteProto.Vote protoVote = VoteProto.Vote.newBuilder()
+                            .setId(savedVote.getId())
+                            .setElectionId(savedVote.getElectionId())
+                            .setBallotId(savedVote.getCartillaId())
+                            .setCandidateId(savedVote.getCandidateId())
+                            .setVoteTime(protoVoteTime)
+                            .build();
+                    return messageService.sendMessage(protoVote).thenReturn(savedVote);
+                });
     }
 }
