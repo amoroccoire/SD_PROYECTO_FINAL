66e72f7df1bc60104584c17cc43d7b712213ac1f
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-22 20:11:54 -0500

    no se toco esto

---
+++ ms-statistics/src/test/java/com/sdg4/ms_statistics/MsStatisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdg4.ms_statistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStatisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
