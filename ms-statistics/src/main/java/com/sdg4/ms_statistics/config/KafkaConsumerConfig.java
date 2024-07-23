40930a92c0dfb3e13930b18016425c94bba8dfad
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-22 20:07:42 -0500

    configuracion del consumidor de kafka

--- ms-statistics/src/main/java/com/sdg4/ms_statistics/config/KafkaConsumerConfig.java
+++ ms-statistics/src/main/java/com/sdg4/ms_statistics/config/KafkaConsumerConfig.java
@@ -1,4 +1,57 @@
 package com.sdg4.ms_statistics.config;
 
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import com.sdg4.ms_statistics.proto.VoteProto;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
 public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+
+    @Bean
+    public ConsumerFactory<String, VoteProto.Vote> consumerConfig() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaProtobufDeserializer.class);
+        configProps.put("schema.registry.url", schemaRegistryUrl); // URL del Schema Registry
+        configProps.put(KafkaProtobufDeserializerConfig.SPECIFIC_PROTOBUF_VALUE_TYPE, VoteProto.Vote.class.getName());
+
+        return new DefaultKafkaConsumerFactory<>(configProps);
+    }
+
+    /*@Bean
+    public ConsumerFactory<String, VoteProto.Vote> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfig());
+    }*/
+
+    //con esto se puede leer, String es la llave, VoteProto.Vote es el value
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, VoteProto.Vote>> consumer() {
+        ConcurrentKafkaListenerContainerFactory<String, VoteProto.Vote> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerConfig());
+        return factory;
+    }
 }
+
