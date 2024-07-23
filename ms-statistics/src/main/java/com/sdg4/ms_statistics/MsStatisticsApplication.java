b038959e8786e73238895388931d2806b8206559
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-22 20:11:02 -0500

    main del microservicio

---
+++ ms-statistics/src/main/java/com/sdg4/ms_statistics/MsStatisticsApplication.java
@@ -0,0 +1,13 @@
+package com.sdg4.ms_statistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStatisticsApplication.class, args);
+	}
+
+}
