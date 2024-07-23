90fcc2adaed3852a7bc20d8b620345d142a6dab3
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2024-07-22 20:02:08 -0500

    repositorio para enviar el voto

--- ms-votes/src/main/java/com/sdg4/votes/repositories/VoteRepository.java
+++ ms-votes/src/main/java/com/sdg4/votes/repositories/VoteRepository.java
@@ -3,8 +3,10 @@ package com.sdg4.votes.repositories;
 import com.sdg4.votes.models.entities.Vote;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
-public interface VoteRepository  extends ReactiveMongoRepository<Vote, Long> {
+public interface VoteRepository  extends ReactiveMongoRepository<Vote, String> {
 
+    Mono<Vote> findByVoterId(String s);
 }
